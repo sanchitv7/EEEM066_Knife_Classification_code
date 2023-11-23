e472d0b6b2055985366818b512cabaa5bf308175
Sanchit Verma <sanchitv7@gmail.com>
2023-11-23 02:09:36 +0000

    checking push from colab

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class DefaultConfigs(object):
     img_weight = 224  # image width
     img_height = 224  # image height
     batch_size = 16  # batch size
-    epochs = 20  # epochs
+    epochs = 10  # epochs
     learning_rate = 0.005  # learning rate
 
 
