c571b7e9a173d178b0241fb84034dbb78294bacf
Sanchit Verma <23355869+sanchitv7@users.noreply.github.com>
2023-11-25 01:40:30 +0000

    Everything put back as is.

---
+++ config.py
@@ -0,0 +1,16 @@
+class DefaultConfigs(object):
+    def __init__(self, n_classes, img_weight, img_height, batch_size, epochs, learning_rate):
+        self.n_classes = n_classes  # number of classes
+        self.img_weight = img_weight  # image width
+        self.img_height = img_height  # image height
+        self.batch_size = batch_size  # batch size
+        self.epochs = epochs  # epochs
+        self.learning_rate = learning_rate  # learning rate
+
+
+config = DefaultConfigs(n_classes=192,
+                        img_weight=224,
+                        img_height=224,
+                        batch_size=16,
+                        epochs=1,
+                        learning_rate=0.005)
